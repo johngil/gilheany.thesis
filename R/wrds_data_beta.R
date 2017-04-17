73da5c6dd51c50e6821351fdeeaa377d72213964
johngil <bballjohngil@gmail.com>
2017-04-16 23:01:29 -0400

    update

--- R/wrds_data_beta.R
+++ R/wrds_data_beta.R
@@ -1,7 +1,7 @@
 # Read in WRDS data for each ticker (Ticker, Price, S&P Return)
 # wrds_data_beta <- c4af81a191300d4d
 # colnames(wrds_data_beta) <- c("ID", "Date","Ticker","Price", "SPR")
-
+data(wrds_data_beta)
 # Convert classes
 library(lubridate)
 wrds_data_beta$Date <- ymd(as.character(wrds_data_beta$Date))
