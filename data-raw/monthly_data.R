41aebd62c8a82a7f4df1753d74540730b9dcef3a
johngil <bballjohngil@gmail.com>
2017-04-19 20:55:37 -0400

    updates

--- data-raw/monthly_data.R
+++ data-raw/monthly_data.R
@@ -1,5 +1,9 @@
 data(monthly_rolling_vol)
 data(monthly_beta_values)
 
-# monthly data for vol and beta
-monthly_data <- merge(monthly_rolling_vol, monthly_beta_values, by = c("Ticker", "Date"))
+book_value <- select(book_value_data, Ticker, Date, PBR1)
+book_value <- book_value[complete.cases(book_value),]
+
+# monthly data for vol and beta and P/B
+monthly_data1 <- merge(monthly_rolling_vol, monthly_beta_values, by = c("Ticker", "Date"), all=TRUE)
+monthly_data2 <- merge(monthly_data1, book_value, by = c("Ticker", "Date"), all=TRUE)
