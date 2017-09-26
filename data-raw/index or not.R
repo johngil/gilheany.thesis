d6ecbdffed93e3adf6e9f4dfd4043e8f4bee4c52
johngil <bballjohngil@gmail.com>
2017-09-25 21:19:51 -0400

    updates

--- data-raw/index or not.R
+++ data-raw/index or not.R
@@ -1,5 +1,5 @@
 # Make a duplicate data frame for monthly
-monthly_test <- monthly
+monthly_test <- monthly5
 
 # Subset minvol data for date, ticker, and weight
 minvol_test <- select(minvol, ticker, date, weight)
@@ -10,10 +10,20 @@ monthly_test1 <- merge(monthly_test, minvol_test, by = c("date", "ticker"), all.
 # If NA means not in min vol index, replace with 0
 monthly_test1$weight[is.na(monthly_test1$weight)] <- 0
 
-# Create new row with 0 or 1 for weight (in min vol index or not)
-monthly_test1$binary <- 0
-monthly_test1$binary[monthly_test1$weight!=0] <- 1
-monthly_test1$binary <- factor(monthly_test1$binary)
+# Create new row with 0 or 1 for weight (in min vol index NOW or not)
+monthly_test1$index_now <- 0
+monthly_test1$index_now[monthly_test1$weight!=0] <- 1
+monthly_test1$index_now <- factor(monthly_test1$index_now)
+
+# Create new row with 0 or 1 for weight (in min vol index BEFORE [6 months ago] or not)
+monthly_test1$index_before <- 0
+monthly_test2 <- monthly_test1
+monthly_test2 <-
+	monthly_test2 %>%
+	group_by(ticker) %>%
+	mutate(index_before = dplyr::lag(index_now, n = 6, default = NA))
+
+monthly_final <- monthly_test2
 
 #building the logit model
 mylogit <- glm(binary ~  beta + volatility + market_value + price_to_book, data = monthly_test1, family = "binomial")
--- data-raw/monthly_data.R
+++ data-raw/monthly_data.R
@@ -7,3 +7,49 @@ book_value <- book_value[complete.cases(book_value),]
 # monthly data for vol and beta and P/B
 monthly_data1 <- merge(monthly_rolling_vol, monthly_beta_values, by = c("Ticker", "Date"), all=TRUE)
 monthly_data2 <- merge(monthly_data1, book_value, by = c("Ticker", "Date"), all=TRUE)
+
+# Create backbone of monthly data set --> subset data with
+monthly1 <- select(usa, ticker, date)
+monthly1$date <- as.Date(monthly1$date)
+monthly_beta_values$ticker <- as.factor(monthly_beta_values$ticker)
+
+# Fill in values for vol, beta --> keep monthly1 same, and fill in with beta values from other data set
+colnames(monthly_beta_values) <- c("date", "ticker", "beta")
+monthly2 <- merge(monthly1, monthly_beta_values, by = c("ticker", "date"), all.x = TRUE)
+#colnames(final_rolling_vol) <- c("date","ticker","volatility")
+monthly3 <- merge(monthly2, final_rolling_vol, by = c("ticker", "date"), all.x = TRUE)
+
+#remove rows with na values in beta or vol
+monthly3 <- monthly3 %>% drop_na(beta)
+monthly3 <- monthly3 %>% drop_na(volatility)
+
+# Fill in for P/B. Lag 3 months
+#book_value_data <- select(book_value_data, date, tic, at, lt, bkvlps, csho, mkvalt, prcc_f)
+#colnames(book_value_data) <- c("date", "ticker", "total_assets", "total_liabilities", "book_value_per_share", "shares_outstanding", "market_value", "stock_price")
+book_value_data1 <- select(book_value_data, Date, Ticker, PBR1)
+#remove rows with na values in P/B
+book_value_data2 <- book_value_data1 %>% drop_na(PBR1)
+#rename column names
+colnames(book_value_data2) <- c("date", "ticker", "price_to_book")
+book_value_data2$date <- ymd(book_value_data2$date)
+
+# Lag each date in book value data set by 3 months
+#book_value_data3 <- book_value_data2 %>% mutate(lag_date = date %m+% months(3))
+book_value_data3$lag_date <- date %m+% months(3)
+book_value_data4 <- select(book_value_data3, lag_date, ticker, price_to_book)
+colnames(book_value_data4) <- c("date", "ticker","price_to_book")
+
+# take backbone of dates and tickers from beta calcs
+book_value_data5 <- select(beta_calcs, Date, Ticker)
+colnames(book_value_data5) <- c("date", "ticker")
+book_value_data6 <- merge(book_value_data5, book_value_data4, by = c("ticker", "date"), all.x = TRUE)
+# fill in NA values for p/b with the most recent value on top
+book_value_data6[1,3] = 0
+book_value_data6$price_to_book <- na.locf(book_value_data6$price_to_book)
+
+# merge p/b data with other monthly data
+monthly4 <- merge(monthly3, book_value_data6, by = c("ticker", "date"), all.x = TRUE)
+monthly5 <- monthly4[!duplicated(monthly4), ]
+
+
+
